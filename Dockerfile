e62ce42520cea40035a73824b47cdc6e56c66c60
leoBitto <leonardobitto@gmail.com>
2024-06-19 16:47:04 +0200

    added pyspark

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,16 @@
 FROM apache/airflow:latest
 
+# Install JDK
+USER root
+RUN apt update \
+  && apt install -y --no-install-recommends \
+  default-jdk \
+  && apt autoremove -yqq --purge \
+  && apt clean \
+  && rm -rf /var/lib/apt/lists/*
+USER airflow
+ENV JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+
 RUN pip install apache-airflow-providers-docker
+COPY requirements.txt .
+RUN pip install -r requirements.txt
--- scripts/scripts1.py
+++ scripts/scripts1.py
@@ -2,13 +2,19 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 import os
-
+from pyspark.sql import SparkSession
 
 
 def task1():
+    spark = SparkSession.builder \
+        .appName("example_pyspark_task") \
+        .getOrCreate()
     PATH_CSV = '/opt/airflow/data/raw'
     NAME_CSV = 'scontrini922.csv'
     COMPLETE_PATH = os.path.join(PATH_CSV, NAME_CSV)
     print(COMPLETE_PATH)
-    df = pd.read_csv(os.path.join(PATH_CSV, NAME_CSV))
+    # Leggi il file CSV
+    df = spark.read.csv(COMPLETE_PATH, header=True, inferSchema=True)
+    # Ferma la sessione Spark
+    spark.stop()
     print(f"the df has been read: {df}")
